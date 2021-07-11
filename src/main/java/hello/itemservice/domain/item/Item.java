d7492ce0eb5a7af76230bc3e17c8c0776fb908c5
anpopo <dkstpgud@gmail.com>
2021-07-11 13:44:20 +0900

    bean validation 프로젝트 셋팅

--- src/main/java/hello/itemservice/domain/item/Item.java
+++ src/main/java/hello/itemservice/domain/item/Item.java
@@ -1,17 +1,32 @@
 package hello.itemservice.domain.item;
 
 import lombok.Getter;
-import lombok.NoArgsConstructor;
 import lombok.Setter;
+import org.hibernate.validator.constraints.Range;
 
+import javax.validation.constraints.Max;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
 import java.util.List;
 
+/**
+ * > javax.validation 으로 시작하면 특정 구현에 관계없이 제공되는 표준 인터페이스이고,
+ * org.hibernate.validator 로 시작하면 하이버네이트 validator 구현체를 사용할 때만 제공되는 검증
+ * 기능이다. 실무에서 대부분 하이버네이트 validator를 사용하므로 자유롭게 사용해도 된다.
+ */
 @Getter @Setter
 public class Item {
 
     private Long id;
+    @NotBlank
     private String itemName;
+
+    @NotNull
+    @Range(min = 1000, max = 1000000)
     private Integer price;
+
+    @NotNull
+    @Max(9999)
     private Integer quantity;
 
     private Boolean open;  // 판매 여부
