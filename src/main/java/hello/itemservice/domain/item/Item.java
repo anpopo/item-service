886576e70d6ce00e7c31d0478b1026ddbaa764c6
anpopo <dkstpgud@gmail.com>
2021-07-11 14:34:11 +0900

    bean validation - group 기능

--- src/main/java/hello/itemservice/domain/item/Item.java
+++ src/main/java/hello/itemservice/domain/item/Item.java
@@ -3,6 +3,7 @@ package hello.itemservice.domain.item;
 import lombok.Getter;
 import lombok.Setter;
 import org.hibernate.validator.constraints.Range;
+import org.hibernate.validator.constraints.ScriptAssert;
 
 import javax.validation.constraints.Max;
 import javax.validation.constraints.NotBlank;
@@ -14,19 +15,23 @@ import java.util.List;
  * org.hibernate.validator 로 시작하면 하이버네이트 validator 구현체를 사용할 때만 제공되는 검증
  * 기능이다. 실무에서 대부분 하이버네이트 validator를 사용하므로 자유롭게 사용해도 된다.
  */
-@Getter @Setter
+@Getter
+@Setter
+//@ScriptAssert(lang = "javascript", script = "_this.price * _this.quantity >= 10000", message = "가격과 수량의 곱은 10000이 넘어야 합니다.")
 public class Item {
 
+    @NotNull(groups = UpdateCheck.class)
     private Long id;
-    @NotBlank
+
+    @NotBlank(groups = {SaveCheck.class, UpdateCheck.class})
     private String itemName;
 
-    @NotNull
-    @Range(min = 1000, max = 1000000)
+    @NotNull(groups = {SaveCheck.class, UpdateCheck.class})
+    @Range(min = 1000, max = 1000000, groups = {SaveCheck.class, UpdateCheck.class})
     private Integer price;
 
-    @NotNull
-    @Max(9999)
+    @NotNull(groups = {SaveCheck.class, UpdateCheck.class})
+    @Max(value = 9999, groups = SaveCheck.class)
     private Integer quantity;
 
     private Boolean open;  // 판매 여부
@@ -34,7 +39,8 @@ public class Item {
     private ItemType itemType;  // 상품종류
     private String deliveryCode;  // 배송 방식
 
-    public Item() {}
+    public Item() {
+    }
 
     public Item(String itemName, Integer price, Integer quantity) {
         this.itemName = itemName;
