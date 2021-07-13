194f5c10c62a43d0dd8b01eddb4a9c93665c36d0
anpopo <dkstpgud@gmail.com>
2021-07-13 14:41:30 +0900

    api 예외 처리

--- src/main/java/hello/itemservice/WebConfig.java
+++ src/main/java/hello/itemservice/WebConfig.java
@@ -1,5 +1,7 @@
 package hello.itemservice;
 
+import hello.itemservice.exception.resolver.MyHandlerExceptionResolver;
+import hello.itemservice.exception.resolver.UserHandlerExceptionResolver;
 import hello.itemservice.web.argumentresolver.LoginMemberArgumentResolver;
 import hello.itemservice.web.filter.LogFilter;
 import hello.itemservice.web.filter.LoginCheckFilter;
@@ -9,6 +11,7 @@ import org.springframework.boot.web.servlet.FilterRegistrationBean;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.method.support.HandlerMethodArgumentResolver;
+import org.springframework.web.servlet.HandlerExceptionResolver;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
@@ -17,6 +20,12 @@ import java.util.List;
 
 @Configuration
 public class WebConfig implements WebMvcConfigurer {
+
+    @Override
+    public void extendHandlerExceptionResolvers(List<HandlerExceptionResolver> resolvers) {
+        resolvers.add(new MyHandlerExceptionResolver());
+        resolvers.add(new UserHandlerExceptionResolver());
+    }
 
     @Override
     public void addArgumentResolvers(List<HandlerMethodArgumentResolver> resolvers) {
@@ -34,7 +43,8 @@ public class WebConfig implements WebMvcConfigurer {
         registry.addInterceptor(new LoginCheckInterceptor())
                 .order(2)
                 .addPathPatterns("/**")
-                .excludePathPatterns("/", "/members/add", "/login", "/logout", "/css/**", "/*.icon", "/error", "/error-**", "/error-**/**");
+                .excludePathPatterns("/", "/members/add", "/login", "/logout", "/css/**", "/*.icon", "/error",
+                        "/error-**", "/error-**/**", "/api/**", "/api/**/**", "/api2/**", "/api2/**/**");
     }
 
 
